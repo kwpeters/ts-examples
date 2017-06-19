baddcc08b576750a4964e6ca3194de6886cdfb0d
Kevin Peters <kwpeters@gmail.com>
2017-06-19 14:29:37 -0400

    Added 03-object-types.ts

---
+++ src/03-object-types.ts
@@ -0,0 +1,20 @@
+
+
+//
+// Defining an object with specific properties.
+//
+const fred: {firstName: string, lastName: string} = {
+    firstName: "Fred",
+    lastName:  "Flintstone"
+};
+
+
+//
+// Defining the key and value types within an object.
+//
+const stateToCapitalMap: {[state: string]: string} = {
+    Alabama: "Montgomery",
+    Alaska: "Juneau",
+    Ohio: 5
+    // ...
+};

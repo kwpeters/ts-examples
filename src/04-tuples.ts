84d97ddd016ecb6bb76bc75a3b5a557681df828c
Kevin Peters <kwpeters@gmail.com>
2017-06-19 14:44:16 -0400

    Added tuples and union types.

---
+++ src/04-tuples.ts
@@ -0,0 +1,16 @@
+// Declare a tuple type
+let x: [string, number];
+
+// Initialize it
+x = ["hello", 10]; // OK
+
+// Initialize it incorrectly
+x = [10, "hello"]; // Error
+
+console.log(x[0].substr(1)); // OK
+console.log(x[1].substr(1)); // Error, 'number' does not have 'substr'
+
+// When accessing an element outside the set of known indices, a union type is used instead:
+x[3] = "world"; // OK, 'string' can be assigned to 'string | number'
+console.log(x[5].toString()); // OK, 'string' and 'number' both have 'toString'
+x[6] = true; // Error, 'boolean' isn't 'string | number'
